590dae76deb1f52095f6a1ca1b17631f63e50f9e
EROUANE237 <erouanetchoumou431@gmail.com>
2025-02-20 11:12:09 +0100

    Delete quiz_app directory

--- quiz_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('ask-question/', views.ai_quiz_ask_question, name='ask_question'),  # Ajouter cette ligne
-    path('answer-question/', views.ai_quiz_answer, name='answer_question'),
-    path('', views.index, name='index'),  # Page d'accueil
-]
